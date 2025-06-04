531550645f86653d9c9e429685e051ed790b642d
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2025-06-04 22:31:16 +0200

    feat: integrate Tailwind CSS, remove unused assets, setup aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      app: '/src/app',
+      entities: '/src/entities',
+      features: '/src/features',
+      widgets: '/src/widgets',
+      pages: '/src/pages',
+      shared: '/src/shared',
+    },
+  },
+});
