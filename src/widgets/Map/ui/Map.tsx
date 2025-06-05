ebf139162e0e4a909ec36bede673ad7bdf0de6e9
Dorian Antoniazzo <dorian.antoniazzo@bebabit.com>
2025-06-05 10:46:27 +0200

    feat: open the map on Buzin

--- src/widgets/Map/ui/Map.tsx
+++ src/widgets/Map/ui/Map.tsx
@@ -1,8 +1,8 @@
-import { useRef, useEffect, useState } from 'react';
-import { Geocoder, useSearchBoxCore } from '@mapbox/search-js-react';
-import mapboxgl, { Map as MapType } from 'mapbox-gl';
-import { env } from 'app/config';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { useRef, useEffect, useState } from "react";
+import { Geocoder, useSearchBoxCore } from "@mapbox/search-js-react";
+import mapboxgl, { Map as MapType } from "mapbox-gl";
+import { env } from "app/config";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export const Map = () => {
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
@@ -11,18 +11,18 @@ export const Map = () => {
     accessToken: env.mapbox.accessToken,
   });
   const [, setMapLoaded] = useState(false);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   useEffect(() => {
     mapboxgl.accessToken = env.mapbox.accessToken;
     if (!mapContainerRef.current) return;
 
     mapInstanceRef.current = new mapboxgl.Map({
       container: mapContainerRef.current, // container ID
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      center: [15.994705, 45.750367], // starting position [lng, lat]
+      zoom: 15, // starting zoom
     });
 
-    mapInstanceRef.current.on('load', () => {
+    mapInstanceRef.current.on("load", () => {
       setMapLoaded(true);
     });
   }, []);
@@ -33,15 +33,15 @@ export const Map = () => {
       <Geocoder
         accessToken={env.mapbox.accessToken}
         onRetrieve={async (e) => {
-          console.log('Retrieved: ', e);
+          console.log("Retrieved: ", e);
 
           const map = mapInstanceRef.current;
           const bbox = e.properties.bbox as [number, number, number, number];
           if (!bbox || !map) return;
-          map.removeLayer('bbox');
-          map.removeLayer('points');
+          map.removeLayer("bbox");
+          map.removeLayer("points");
           const bboxJson = {
-            type: 'Polygon',
+            type: "Polygon",
             coordinates: [
               [
                 [bbox[0], bbox[1]],
@@ -53,36 +53,36 @@ export const Map = () => {
               ],
             ],
           };
-          map.addSource('bbox', {
-            type: 'geojson',
+          map.addSource("bbox", {
+            type: "geojson",
             data: {
-              type: 'Feature',
+              type: "Feature",
               /* @ts-expect-error typing issue, it's ok */
               geometry: bboxJson,
             },
           });
 
           map.addLayer({
-            id: 'bbox',
-            type: 'fill',
-            source: 'bbox',
+            id: "bbox",
+            type: "fill",
+            source: "bbox",
             layout: {},
             paint: {
-              'fill-color': '#088',
-              'fill-opacity': 0.25,
+              "fill-color": "#088",
+              "fill-opacity": 0.25,
             },
           });
-          const restaurants = await searchBoxCore.category('food_and_drink', {
+          const restaurants = await searchBoxCore.category("food_and_drink", {
             bbox,
             limit: 25,
             /*  proximity: e.geometry.coordinates as [number, number], */
           });
-          console.log('Restaurants: ', restaurants);
+          console.log("Restaurants: ", restaurants);
 
-          map.addSource('points', {
-            type: 'geojson',
+          map.addSource("points", {
+            type: "geojson",
             data: {
-              type: 'FeatureCollection',
+              type: "FeatureCollection",
               features: restaurants.features,
             },
           });
@@ -91,18 +91,18 @@ export const Map = () => {
           // Routes within Colorado have 'circle-color': '#f55442'
           // Fallback values (routes not within Colorado) have 'circle-color': '#484848'
           map.addLayer({
-            id: 'points',
-            type: 'circle',
-            source: 'points',
+            id: "points",
+            type: "circle",
+            source: "points",
             layout: {},
             paint: {
-              'circle-color': [
-                'case',
-                ['within', bboxJson],
-                '#f55442',
-                '#484848',
+              "circle-color": [
+                "case",
+                ["within", bboxJson],
+                "#f55442",
+                "#484848",
               ],
-              'circle-radius': 10,
+              "circle-radius": 10,
             },
           });
         }}
